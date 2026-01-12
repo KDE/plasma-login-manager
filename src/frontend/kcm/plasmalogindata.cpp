22625ec7b19cc72c8bc62e80cb66dd19e3c72d50
Oliver Beard <olib141@outlook.com>
2026-01-12 12:00:42 +0000

    kcm: Hide when plasmalogin is not enabled
    Hide the Login Screen KCM when plasmalogin's systemd service is disabled.

--- src/frontend/kcm/plasmalogindata.cpp
+++ src/frontend/kcm/plasmalogindata.cpp
@@ -5,6 +5,10 @@
  *  SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include <QDBusConnection>
+#include <QDBusMessage>
+#include <QDBusReply>
+
 #include "plasmaloginsettings.h"
 
 #include "plasmalogindata.h"
@@ -13,6 +17,22 @@ PlasmaLoginData::PlasmaLoginData(QObject *parent)
     : KCModuleData(parent)
     , m_wallpaperSettings(new WallpaperSettings(this))
 {
+    QDBusMessage msg = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.systemd1"),
+                                                      QStringLiteral("/org/freedesktop/systemd1"),
+                                                      QStringLiteral("org.freedesktop.systemd1.Manager"),
+                                                      QStringLiteral("GetUnitFileState"));
+    msg << QStringLiteral("plasmalogin.service");
+
+    // it seems system settings wants things to be sync
+    // It's not like systemd will ever be down
+    QDBusReply<QString> hasPlasmaLoginReply = QDBusConnection::systemBus().call(msg);
+    // a quirk is that if plasmalogin is uninstalled systemd replies with an error of invalid args
+    if (hasPlasmaLoginReply.value() == QLatin1String("enabled")) {
+        setRelevant(true);
+    } else {
+        setRelevant(false);
+    }
+
     m_wallpaperSettings->load();
 }
 
