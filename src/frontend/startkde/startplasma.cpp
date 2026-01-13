a23be40a366ca045db3fafd9654b6537027dcd88
David Edmundson <kde@davidedmundson.co.uk>
2026-01-13 10:24:40 +0000

    frontend: Force the breeze QQC2 style
    Since the plasma QPT is not loaded this causes our colour schemes
    to not load.

--- src/frontend/startkde/startplasma.cpp
+++ src/frontend/startkde/startplasma.cpp
@@ -261,7 +261,7 @@ void setupPlasmaEnvironment()
     // Manually disable auto scaling because we are scaling above
     // otherwise apps that manually opt in for high DPI get auto scaled by the developer AND manually scaled by us
     qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "0");
-
+    qputenv("QT_QUICK_CONTROLS_STYLE", "org.kde.breeze");
     qputenv("KDE_APPLICATIONS_AS_SCOPE", "1");
 
     // Add kdedefaults dir to allow config defaults overriding from a writable location
