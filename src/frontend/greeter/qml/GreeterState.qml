KDE/plasma-login-manager

commit 61a7a1f7e47bf3994771ba0d2bab6880faaa050e

frontend/greeter: Handle timeout in state singleton
This vastly simplifies the relevant code.

src/frontend/greeter/qml/GreeterState.qml
@@ -32,9 +32,7 @@ Item {
 
     readonly property var activeWindow: internal.activeWindow
 
-    // TODO: In Login.qml, identify which LoginState we represent and have connections on GreeterState
-    //       property changed and update GreeterState property when the relevant thing is changed
-    //       also property for session combo
+    // Shared state
 
     readonly property int beyondUserLimit: PlasmaLogin.UserModel.rowCount() > 7
 
@@ -75,6 +73,41 @@ Item {
 
     property bool showPassword: false
 
+    // Shared functionality
+
+    readonly property bool inhibitGreeterTimeout: {
+        if (greeterState.loginState === PlasmaLogin.GreeterState.LoginState.UserList && greeterState.userListPassword.length > 0) {
+            // We're on the user list and a password is entered
+            return true;
+        } else if (greeterState.loginState === PlasmaLogin.GreeterState.UserPrompt && greeterState.userPromptPassword.length > 0) {
+            // We're on the user prompt and a password is entered
+            return true;
+        }
+
+        // inputPanel.keyboardActive
+
+        // No reason to block timeout
+        return false;
+    }
+    onInhibitGreeterTimeoutChanged: {
+        let greeterShouldTimeOut = greeterState.activeWindow !== null;
+        if (greeterTimeoutTimer.running !== greeterShouldTimeOut) {
+            greeterTimeoutTimer.running = greeterShouldTimeOut;
+        }
+    }
+
+    Timer {
+        id: greeterTimeoutTimer
+        running: false
+        interval: 10000
+        onTriggered: {
+            if (internal.activeWindow) {
+                greeterState.showPassword = false;
+                timeoutWindow(internal.activeWindow);
+            }
+        }
+    }
+
     function clearPasswords(): void {
         userListPassword = "";
         userPromptPassword = "";
@@ -86,13 +119,23 @@ Item {
         }
 
         internal.activeWindow = window;
+
+        window.requestActivate();
+
+        if (!inhibitGreeterTimeout) {
+            greeterTimeoutTimer.restart();
+        }
     }
 
     function timeoutWindow(window): void {
         if (internal.activeWindow == window) {
             internal.activeWindow = null;
         }
+
+        greeterTimeoutTimer.stop();
     }
+
+    // Remember last logged in user/session
 
     property string lastLoggedInUser
     property string lastLoggedInSession

src/frontend/greeter/qml/Main.qml
@@ -49,11 +49,11 @@ Item {
         function onKeyPressed(): void {
             // callLater, as otherwise 'enter' key press would arrive after waking
             // and the uiVisible check would pass and a login attempt would be made
-            Qt.callLater(() => loginScreenRoot.wake());
+            Qt.callLater(() => PlasmaLogin.GreeterState.activateWindow(loginScreenRoot.Window.window));
         }
 
         function onEscapeKeyPressed(): void {
-            loginScreenRoot.timeout();
+            PlasmaLogin.GreeterState.timeoutWindow(loginScreenRoot.Window.window);
             /*
 -            if (inputPanel.keyboardActive) {
 -                inputPanel.showHide();
@@ -68,66 +68,13 @@ Item {
         anchors.fill: parent
 
         hoverEnabled: true
-        cursorShape: uiVisible ? Qt.ArrowCursor : Qt.BlankCursor
 
         property bool uiVisible: PlasmaLogin.GreeterState.activeWindow === Window.window
-        property bool blockUiTimeout: {
-            if (PlasmaLogin.GreeterState.loginState === PlasmaLogin.GreeterState.LoginState.UserList && PlasmaLogin.GreeterState.userListPassword.length > 0) {
-                // We're on the user list and a password is entered
-                return true;
-            } else if (PlasmaLogin.GreeterState.loginState === PlasmaLogin.GreeterState.UserPrompt && PlasmaLogin.GreeterState.userPromptPassword.length > 0) {
-                // We're on the user prompt and a password is entered
-                return true;
-            }
 
-            // inputPanel.keyboardActive
+        cursorShape: uiVisible ? Qt.ArrowCursor : Qt.BlankCursor
 
-            // No reason to block timeout
-            return false;
-        }
-
-        function wake() {
-            PlasmaLogin.GreeterState.activateWindow(Window.window);
-        }
-
-        function timeout() {
-            PlasmaLogin.GreeterState.timeoutWindow(Window.window);
-        }
-
-        onPressed: wake()
-        onPositionChanged: wake()
-
-        onUiVisibleChanged: {
-            if (uiVisible) {
-                Window.window.requestActivate();
-            }
-
-            if (blockUiTimeout) {
-                uiTimeoutTimer.running = false;
-            } else if (uiVisible) {
-                uiTimeoutTimer.restart();
-            }
-        }
-
-        onBlockUiTimeoutChanged: {
-            if (blockUiTimeout) {
-                uiTimeoutTimer.running = false;
-            } else if (uiVisible) {
-                uiTimeoutTimer.restart();
-            }
-        }
-
-        Timer {
-            id: uiTimeoutTimer
-            running: false
-            interval: 10000
-            onTriggered: {
-                if (!loginScreenRoot.blockUiTimeout) {
-                    userListComponent.mainPasswordBox.showPassword = false;
-                    loginScreenRoot.timeout();
-                }
-            }
-        }
+        onPressed: PlasmaLogin.GreeterState.activateWindow(Window.window);
+        onPositionChanged: PlasmaLogin.GreeterState.activateWindow(Window.window);
 
         DropShadow {
             id: clockShadow

commit a90682e2dde21dd05571487727e7b6e9e1155fa4

frontend/greeter: Fix greeter timing out when should be blocked
Due to a regression in 61a7a1f7, a missing condition meant that the greeter would still time out when it should be blocked from doing so, if the timeout timer were already running.

src/frontend/greeter/qml/GreeterState.qml
@@ -90,7 +90,7 @@ Item {
         return false;
     }
     onInhibitGreeterTimeoutChanged: {
-        let greeterShouldTimeOut = greeterState.activeWindow !== null;
+        let greeterShouldTimeOut = greeterState.activeWindow !== null && !greeterState.inhibitGreeterTimeout;
         if (greeterTimeoutTimer.running !== greeterShouldTimeOut) {
             greeterTimeoutTimer.running = greeterShouldTimeOut;
         }